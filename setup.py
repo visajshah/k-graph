837ffbd07f5eff824531239438e5e813da46f4be
Visaj Nirav Shah <visaj.shah@gmail.com>
2022-12-08 14:41:49 -0700

    Add kgraph

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import find_packages, setup
 setup(
     name='kgraph-lib',
     packages=find_packages(include = ['kgraph']),
-    version='0.1.0',
+    version='0.1.1',
     description='k-means reimagined for graphs',
     author='Visaj Nirav Shah',
     license='MIT',
-    install_requires = ['networkx', 'pandas', 'numpy', 'scikit-learn']
+    install_requires = ['networkx', 'pandas', 'numpy', 'scikit-learn', 'scipy==1.8.1']
 )
